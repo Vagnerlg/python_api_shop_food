c754ed48e6ca070e941ae28fccd651dee13b09f0
Vagner Goncalves <vlgonalves@gmail.com>
2022-11-22 23:32:28 -0300

    test: class transform

--- shop_food/infra/database/util/parse.py
+++ shop_food/infra/database/util/parse.py
@@ -8,12 +8,15 @@ class Transform:
         model['updated_at'] = datetime.now()
 
     def merge(self, current: dict, new_model: dict) -> dict[str, Any]:
-        del new_model['created_at']
-        del new_model['updated_at']
+        if new_model.get('created_at'):
+            del new_model['created_at']
+        if new_model.get('updated_at'):
+            del new_model['updated_at']
         self.set_date(current)
 
         prod_dict = current | new_model
-        del prod_dict['id']
+        if prod_dict.get('id'):
+            del prod_dict['id']
 
         return prod_dict
 
@@ -33,7 +36,7 @@ class Transform:
                 result[key] = items
                 continue
 
-            if original[key] and key != '_id':
+            if original[key] is not None and key != '_id':
                 result[key] = original[key]
                 continue
 
@@ -42,17 +45,10 @@ class Transform:
 
         return result
 
-    def verify_relation(self, model: str) -> bool:
-        collections = [
-            'category',
-            'product'
-        ]
-        return model in collections
-
     def prepare_obj(self, original: dict) -> dict:
         for key in original.copy():
             if isinstance(original[key], dict):
-                if self.verify_relation(key):
+                if key in ['category', 'product']:
                     original[f'{key}_id'] = original[key]['id']
                     del original[key]
                     continue
---
+++ tests/unit/infra/database/util/parse_test.py
@@ -0,0 +1,145 @@
+from datetime import datetime
+import pytest
+
+from shop_food.app import load_app
+from shop_food.infra.database.util.parse import Transform
+from bson import ObjectId
+
+
+@pytest.fixture
+def transform():
+    fi = load_app('unit_testing')
+    return fi.injector.get(Transform)
+
+
+@pytest.fixture
+def dict_object():
+    return {
+        'id': '6375a88f4f117d8c1d931b09',
+        'created_at': datetime(2022, 11, 17, 0, 20, 47, 778000),
+        'user': {
+            'created_at': datetime(2022, 11, 17, 0, 20, 47, 778000),
+            'email': 'vlgonalves@gmail.com'
+        },
+        'items': [
+            {
+                'created_at': datetime(2022, 11, 17, 0, 20, 47, 778000),
+                'quantity': 1,
+                'product': {
+                    'id': '63724a1c7254bc2c84c02d76',
+                    'created_at': datetime(2022, 11, 14, 11, 1, 0, 976000),
+                    'title': 'Pizza Calabresa',
+                    'category': {
+                        'id': '636c72cf6ca49b5bd1234fa8',
+                        'created_at': datetime(2022, 11, 10, 0, 41, 3, 190000),
+                        'name': 'Pizzas'
+                    }
+                }
+            }
+        ],
+        'status': 'created'
+    }
+
+
+@pytest.fixture
+def dict_database():
+    return {
+        'id': '6375a88f4f117d8c1d931b09',
+        'created_at': datetime(2022, 11, 17, 0, 20, 47, 778000),
+        'user': {
+            'created_at': datetime(2022, 11, 17, 0, 20, 47, 778000),
+            'email': 'vlgonalves@gmail.com'
+        },
+        'items': [
+            {
+                'created_at': datetime(2022, 11, 17, 0, 20, 47, 778000),
+                'quantity': 1,
+                'product_id': '63724a1c7254bc2c84c02d76'
+            }
+        ],
+        'status': 'created'
+    }
+
+
+def test_set_date(transform: Transform) -> None:
+    result = {}
+    transform.set_date(result)
+    updated_at = result.get('updated_at')
+
+    assert updated_at is not None
+    assert isinstance(updated_at, datetime)
+
+
+def test_merge_empty(transform: Transform) -> None:
+    result = transform.merge({}, {})
+
+    updated_at = result.get('updated_at')
+
+    assert updated_at is not None
+    assert isinstance(updated_at, datetime)
+
+
+def test_merge_full(transform: Transform) -> None:
+    result = transform.merge({
+        'foo': 'foo',
+        'field': 'old'
+    }, {
+        'bar': 'bar',
+        'field': 'new'
+    })
+
+    expected = {
+        'bar': 'bar',
+        'field': 'new',
+        'foo': 'foo'
+    }
+
+    del result['updated_at']
+
+    assert expected == result
+
+
+def test_prepare_obj(transform: Transform, dict_object: dict, dict_database: dict):
+    result = transform.prepare_obj(dict_object)
+
+    assert result == dict_database
+
+
+def test_prepare_model(transform: Transform):
+    result = transform.prepare_model({
+        '_id': ObjectId('63724a1c7254bc2c84c02d76'),
+        'other_dict': {
+            '_id': ObjectId('63724a1c7254bc2c84c02d76'),
+            'foo': True,
+            'bar': None
+        },
+        'items': [
+            {
+                '_id': ObjectId('63724a1c7254bc2c84c02d76'),
+                'bar': True
+            },
+            {
+                '_id': ObjectId('63724a1c7254bc2c84c02d76'),
+                'bar': False
+            }
+        ]
+    })
+    expected = {
+        'id': '63724a1c7254bc2c84c02d76',
+        'other_dict': {
+            'id': '63724a1c7254bc2c84c02d76',
+            'foo': True
+        },
+        'items': [
+            {
+                'id': '63724a1c7254bc2c84c02d76',
+                'bar': True
+            },
+            {
+                'id': '63724a1c7254bc2c84c02d76',
+                'bar': False
+            }
+        ]
+    }
+
+    assert expected == result
